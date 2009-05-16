e9e389d97124be4e077d593f26c7e611303e5e5c
Josh Betz <j@joshbetz.com>
2009-05-16 17:05:22 +0000

    tweet added settings for debugging

--- hsr-settings.php
+++ hsr-settings.php
@@ -1,6 +1,4 @@
-<?php			
-	error_reporting(E_ALL ^ E_NOTICE);
-	//error_reporting(E_ALL);
+<?php
 	
 	require('hsr-includes/functions.php');
 	require('hsr-includes/vars.php');
@@ -13,5 +11,13 @@
 	define('VERSION', '.8'); // HSR Version
 	define('BETABUILD', true); // is this a beta?
 	define('BUILDNUMBER', '79'); // HSR Build Number
+	
+	define('DEBUGGING', false);
+	
+	if(DEBUGGING) {
+		error_reporting(E_ALL);
+	} else {
+		error_reporting(E_ALL ^ E_NOTICE);
+	}
 
 ?>
